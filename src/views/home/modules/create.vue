baf9cc1145ab54eb7729fa9ddfca5ded573d74d5
cuberxp <2317537731@qq.com>
2021-05-19 02:50:36 +0800

    add create activity

--- src/views/home/modules/create.vue
+++ src/views/home/modules/create.vue
@@ -14,20 +14,24 @@
           <el-row>
             <el-col :span="19" :offset="1">
               <el-form-item label="活动名称">
-                <el-input placeholder="请输入活动标题"></el-input>
+                <el-input
+                  placeholder="请输入活动标题"
+                  v-model="baseSetting.title"
+                ></el-input>
               </el-form-item>
             </el-col>
 
             <el-col :span="8" :offset="1">
               <el-form-item label="活动时间">
                 <el-date-picker
-                  v-model="value2"
+                  v-model="activityDate"
                   type="datetimerange"
                   :picker-options="pickerOptions"
                   range-separator="至"
                   start-placeholder="开始时间"
                   end-placeholder="结束时间"
                   align="right"
+                  @change="selectActvityDate"
                 >
                 </el-date-picker>
               </el-form-item>
@@ -38,7 +42,7 @@
                 ref="md"
                 @imgAdd="$imgAdd"
                 @imgDel="$imgDel"
-                v-model="originalContent"
+                v-model="baseSetting.ruleContent"
                 :subfield="props.subfield"
                 :defaultOpen="props.defaultOpen"
                 :editable="props.editable"
@@ -62,8 +66,8 @@
             <span class="activity_image_title2">活动封面只能选择一种方式</span>
           </el-col>
           <el-col :span="22" :offset="1">
-            <el-tabs v-model="activeName" @tab-click="handleClick">
-              <el-tab-pane label="图片封面" name="first">
+            <el-tabs v-model="styleSetting.coverType" @tab-click="handleClick">
+              <el-tab-pane label="图片封面" name="0">
                 <el-upload
                   :action="uploadURL"
                   :on-preview="handlePreview"
@@ -78,8 +82,11 @@
                   </div>
                 </el-upload>
               </el-tab-pane>
-              <el-tab-pane label="视频封面" name="second">
-                <el-input placeholder="请输入BiliBili视频链接"></el-input>
+              <el-tab-pane label="视频封面" name="1">
+                <el-input
+                  placeholder="请输入BiliBili视频链接"
+                  v-model="styleSetting.videoCover"
+                ></el-input>
                 <el-link type="primary" href="www.baidu.com"
                   >目前仅支技BiliBili视频链接，如何上传视频链接？</el-link
                 >
@@ -90,16 +97,16 @@
           <el-col :span="19" :offset="1" class="noticeDiv">
             <span class="noticeStyle">活动公告</span>
             <el-switch
-              v-model="noticeFlag"
+              v-model="styleSetting.noticeFlag"
               active-text="开启"
               inactive-text="关闭"
             >
             </el-switch>
             <el-input
               class="inputNotice"
-              v-if="noticeFlag"
+              v-if="styleSetting.noticeFlag"
               placeholder="请输入活动公告"
-              v-model="notice"
+              v-model="styleSetting.notice"
             ></el-input>
             <p class="noticeRemakr">可以帮助用户宣传</p>
           </el-col>
@@ -107,23 +114,35 @@
           <el-col :span="23" :offset="1">
             <h5>按票按钮</h5>
             <div class="voteButtonsGroup">
-              <el-radio-group v-model="voteButtonType" class="voteType">
+              <el-radio-group
+                v-model="styleSetting.voteButtonType"
+                class="voteType"
+              >
                 <el-radio :label="1">点赞</el-radio>
                 <el-radio :label="3">投票</el-radio>
                 <el-radio :label="2">打分</el-radio>
               </el-radio-group>
 
-              <div v-if="voteButtonType == 1" class="voteButtPrise">
+              <div
+                v-if="styleSetting.voteButtonType == 1"
+                class="voteButtPrise"
+              >
                 <el-button type="primary" size="medium" plain>点赞</el-button>
               </div>
 
-              <div v-if="voteButtonType == 3" class="voteButtPrise">
+              <div
+                v-if="styleSetting.voteButtonType == 3"
+                class="voteButtPrise"
+              >
                 <el-button type="success" size="medium" plain>投票</el-button>
               </div>
 
-              <div v-if="voteButtonType == 2" class="voteButtPrise2">
+              <div
+                v-if="styleSetting.voteButtonType == 2"
+                class="voteButtPrise2"
+              >
                 <el-input-number
-                  v-model="beginNum"
+                  v-model="styleSetting.inputRangeBegin"
                   :precision="2"
                   :step="0.1"
                   :max="1000"
@@ -131,7 +150,7 @@
                 ></el-input-number>
                 ~
                 <el-input-number
-                  v-model="endNum"
+                  v-model="styleSetting.inputRangeEnd"
                   :precision="2"
                   :step="0.1"
                   :max="1000"
@@ -150,25 +169,21 @@
       <div v-show="active === 2">
         <el-col :span="19" :offset="1">
           <span class="noticeStyle">是否开启选手自主报名 </span>
-          <el-switch
-            v-model="playerSignFlag"
-            active-text="开启"
-            inactive-text="关闭"
-          >
-          </el-switch>
+          <el-radio-group size="small" v-model="functionSetting.signFlag">
+            <el-radio-button :label="0">关闭</el-radio-button>
+            <el-radio-button :label="1">开启</el-radio-button>
+          </el-radio-group>
         </el-col>
-        <el-col :span="19" :offset="1" v-if="playerSignFlag">
+        <el-col :span="19" :offset="1" v-if="functionSetting.signFlag">
           <span class="noticeStyle">报名审核</span>
-          <el-switch
-            v-model="playerSignReviewFlag"
-            active-text="开启"
-            inactive-text="关闭"
-          >
-          </el-switch>
+          <el-radio-group size="small" v-model="functionSetting.singUpFlag">
+            <el-radio-button :label="0">关闭</el-radio-button>
+            <el-radio-button :label="1">开启</el-radio-button>
+          </el-radio-group>
         </el-col>
         <el-col :span="23" :offset="1" style="margin-top: 10px">
           <span class="noticeStyle">选手排序 </span>
-          <el-radio-group size="small" v-model="playerSortMethod">
+          <el-radio-group size="small" v-model="functionSetting.playerMethod">
             <el-radio-button label="1">编号从低到高</el-radio-button>
             <el-radio-button label="2">票数从高到低</el-radio-button>
             <el-radio-button label="3">报名时间倒序</el-radio-button>
@@ -177,51 +192,85 @@
         <el-col :span="23" :offset="1">
           <span class="noticeStyle">分组是否开启</span>
           <el-switch
-            v-model="playerGroupFlag"
+            v-model="functionSetting.groupFlag"
             active-text="开启"
             inactive-text="关闭"
           >
           </el-switch>
           <span class="noticeGroupRemark">（选填）选手报名和排行进行分组</span>
-          <div v-if="playerGroupFlag" style="margin-top: 10px">
+          <div v-if="functionSetting.groupFlag" style="margin-top: 10px">
             <el-button
               size="small"
               type="danger"
               @click="addGroupDialogVisible = true"
               >增加</el-button
             >
-            <el-table :data="group" style="width: 100%">
+            <el-table :data="functionSetting.group" style="width: 100%">
               <el-table-column prop="id" label="编号" width="180">
               </el-table-column>
 
               <el-table-column prop="name" label="分组名称" width="180">
               </el-table-column>
               <el-table-column label="操作" width="180">
-                <template>
+                <template slot-scope="scope">
                   <el-button
                     type="success"
-                    @click="updateGroupDialogVisible = true"
+                    @click="setUpdateDigalVisital(scope.row.id)"
                     plain
                     size="mini"
                     >修改</el-button
                   >
-                  <el-button type="danger" size="mini" plain>删除</el-button>
+                  <el-button
+                    type="danger"
+                    size="mini"
+                    @click="delGroup(scope.row.id)"
+                    plain
+                    >删除</el-button
+                  >
                 </template>
               </el-table-column>
             </el-table>
           </div>
         </el-col>
 
-        <el-col :span="23" :offset="1" style="margin-top: 10px">
-          <h6>投票规则</h6>
-          <el-radio-group size="small" v-model="playerSortMethod">
-            <el-radio-button label="1">编号从低到高</el-radio-button>
-            <el-radio-button label="2">票数从高到低</el-radio-button>
-            <el-radio-button label="3">报名时间倒序</el-radio-button>
+        <el-col :span="23" :offset="1">
+          <p class="voetRoleTile">投票规则</p>
+          <el-radio-group v-model="functionSetting.voteRuleType">
+            <el-radio :label="1"
+              >一个号每天能投(打)
+
+              <el-input-number
+                v-model="functionSetting.playerVoteCount"
+                controls-position="right"
+                @change="handleChange"
+                :min="1"
+                :max="100"
+                size="small"
+                class="everyDayNumStyle"
+              ></el-input-number>
+              票(次打分)，可将全部票投给同一个选手</el-radio
+            >
+            <el-radio :label="2" style="margin-top: 10px"
+              >活动时间内只能投(打)一票(次打分)</el-radio
+            >
           </el-radio-group>
         </el-col>
-        <el-col :span="23" offset="1">
-          <el-button type="success" plain>创建活动</el-button>
+        <el-col :span="19" :offset="1">
+          <span class="noticeStyle">是否开启是否开启匿名投票</span>
+          <el-radio-group size="small" v-model="functionSetting.secretVoteFlag">
+            <el-radio-button :label="0">关闭</el-radio-button>
+            <el-radio-button :label="1">开启</el-radio-button>
+          </el-radio-group>
+          <p class="noticeRemakr">无需登陆就可投票</p>
+        </el-col>
+        <el-col
+          :span="14"
+          offset="9"
+          style="margin-top: 10px; margin-bottom: 10px"
+        >
+          <el-button type="success" @click="creatActivity" plain
+            >创建活动</el-button
+          >
         </el-col>
       </div>
     </div>
@@ -232,12 +281,13 @@
       width="30%"
       :before-close="handleClose"
     >
-      <el-input placeholder="输入修改后的分组名称"></el-input>
+      <el-input
+        placeholder="输入修改后的分组名称"
+        v-model="updateGroupItem.name"
+      ></el-input>
       <span slot="footer" class="dialog-footer">
         <el-button @click="updateGroupDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false"
-          >确 定</el-button
-        >
+        <el-button type="primary" @click="updateGroup">确 定</el-button>
       </span>
     </el-dialog>
 
@@ -245,14 +295,11 @@
       title="添加分组名称"
       :visible.sync="addGroupDialogVisible"
       width="30%"
-      :before-close="handleClose"
     >
-      <el-input placeholder="输入组名称"></el-input>
+      <el-input placeholder="输入组名称" v-model="groupName"></el-input>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addGroupDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false"
-          >确 定</el-button
-        >
+        <el-button type="primary" @click="addGroup">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -262,9 +309,7 @@
 export default {
   data() {
     return {
-      value2: "",
       active: 0,
-      originalContent: "",
       activeName: "first",
       // 已选步骤
       stepSuc: [0],
@@ -307,31 +352,66 @@ export default {
           },
         ],
       },
-      noticeFlag: true,
-      notice: "",
-      beginNum: 0.0,
-      endNum: 100,
-      voteButtonType: 1,
-      playerSignFlag: true,
-      playerSignReviewFlag: true,
-      playerGroupFlag: true,
+      uploadURL: "http://localhost:8081/user/avatar",
+      headerObj: {
+        t: window.localStorage.getItem("token"),
+      },
+      srcList: [],
+      previcewPath: "",
+      previewDialogVisible: true,
+      groupName: "",
+      groupActivityId: 0,
       updateGroupDialogVisible: false,
       addGroupDialogVisible: false,
-      group: [
-        {
-          id: 1,
-          name: "test",
-        },
-        {
-          id: 1,
-          name: "test",
-        },
-        {
-          id: 1,
-          name: "test",
-        },
-      ],
-      playerSortMethod: 2,
+      activityDate: [],
+      updateGroupItem: {
+        id: 0,
+        name: "",
+      },
+      //基础设置相关参数
+      baseSetting: {
+        title: "",
+        beginTime: 0,
+        endTime: 0,
+        ruleContent: "",
+      },
+      styleSetting: {
+        // 0 图片封面 1视频
+        coverType: "0",
+        videoCover: "",
+        // 图片封南
+        imgsCover: [],
+        //活动公告
+        noticeFlag: false,
+        notice: "",
+        // 投票按钮类型
+        // 1点赞，2投票，3打分
+        voteButtonType: 1,
+        // 打分的话他的范围是多少
+        inputRangeBegin: 0,
+        inputRangeEnd: 100,
+      },
+      functionSetting: {
+        // 是否开启选手自主报名
+        // 0 关闭 1开启
+        signFlag: 0,
+        // 报名审核
+        //  0 关闭 1开启
+        singUpFlag: 0,
+        //选手排序
+        // 1 编号从低到高
+        // 2  票数从高到低
+        // 3 报名时间倒序
+        playerMethod: 1,
+        groupFlag: true,
+        //分组数据
+        group: [],
+        // 1一个号每天能投，2活动时间内只能投
+        voteRuleType: 1,
+        playerVoteCount: 1,
+        // 0 关闭 1开记
+        secretVoteFlag: 0,
+      },
     };
   },
   methods: {
@@ -349,11 +429,106 @@ export default {
     },
     // 组件点击下一步
     handleNextStep() {
+      // 这里可以加上参数检验
+      console.log(this.baseSetting);
+      console.log(this.styleSetting);
+      console.log(this.functionSetting);
       this.stepSuc.push(++this.active);
     },
     // 绑定@imgAdd event
     async $imgAdd(pos, $file) {},
     $imgDel(pos) {},
+    selectActvityDate(date) {
+      this.baseSetting.beginTime = date[0].valueOf();
+      this.baseSetting.endTime = date[1].valueOf();
+
+      console.log(this.baseSetting);
+    },
+    // 处理图片预览效果
+    handlePreview(file) {
+      this.previcewPath = file.response.data;
+      this.previewDialogVisible = true;
+    },
+    // 处理移除图片的操作
+    handleRemove(file) {
+      const filePath = file.response.data;
+      const i = this.styleSetting.imgsCover.findIndex((x) => x === filePath);
+
+      this.styleSetting.imgsCover.splice(i, 1);
+      console.log(this.addForm);
+    },
+    // 监听图片上传成功
+    handleSuccess(response) {
+      if (response.code !== 200) {
+        return this.$message.error("上传文件失败!" + response.message);
+      }
+
+      console.log(response);
+      // 2.将图片信息对象push 到 pics 数组中
+      this.styleSetting.imgsCover.push(response.data);
+      this.srcList = new Array(response.data, response.data);
+    },
+    addGroup() {
+      let groupObject = {
+        id: this.groupActivityId++,
+        name: this.groupName,
+      };
+      this.functionSetting.group.push(groupObject);
+      this.addGroupDialogVisible = false;
+      this.groupName = "";
+    },
+    delGroup(id) {
+      const index = this.functionSetting.group.findIndex(
+        (item) => item.id === id
+      );
+
+      this.functionSetting.group.splice(index, 1);
+    },
+    setUpdateDigalVisital(id) {
+      this.updateGroupItem = this.functionSetting.group.find(
+        (item) => item.id === id
+      );
+
+      this.updateGroupDialogVisible = true;
+    },
+    updateGroup() {
+      this.functionSetting.group.forEach((item) => {
+        if (item.id === this.updateGroupItem.id) {
+          item.name = this.updateGroupItem.name;
+        }
+      });
+      this.updateGroupDialogVisible = false;
+    },
+    // 创建活动
+    async creatActivity() {
+      console.log(this.baseSetting);
+      console.log(this.styleSetting);
+      console.log(this.functionSetting);
+
+      let targetActivity = Object.assign(
+        this.baseSetting,
+        this.styleSetting,
+        this.functionSetting
+      );
+
+      targetActivity.coverType = Number.parseInt(targetActivity.coverType);
+      targetActivity.group = targetActivity.group.map((item) => item.name);
+      targetActivity.createId = window.localStorage.getItem("id");
+      console.log(targetActivity);
+
+      const { data: res } = await this.$http.post(
+        "/activity/create",
+        targetActivity
+      );
+
+      if (res.code !== 200) {
+        return this.$message.error(res.message);
+      }
+
+      this.$message.success("创建活动成功!");
+      this.$router.push("/manage/votes_manage");
+      this.$router.go(0);
+    },
   },
 };
 </script>
@@ -373,7 +548,7 @@ export default {
 .steps {
   width: 600px;
   min-height: 600px;
-  max-height: 1000px;
+  max-height: 2000px;
   background: #fff;
   margin: 0 auto;
   box-shadow: 0px 4px 17px 1px rgb(0 0 0 / 15%);
@@ -445,5 +620,13 @@ export default {
 .noticeGroupRemark {
   font-size: 12px;
   color: #9b9b9b;
+}
+.everyDayNumStyle {
+  width: 85px;
+  height: 30px;
+}
+.voetRoleTile {
+  font-size: 13px;
+  font-weight: bold;
 }
 </style>
